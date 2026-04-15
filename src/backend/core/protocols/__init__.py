669c9cffba427c1d61497d81ce69f7998117650e
653o <s.j.s.tlswnstj@gmail.com>
2026-04-16 03:27:02 +0900

    chore: organize folders structurally and cleanly, ignore node_modules

--- src/backend/core/protocols/__init__.py
+++ src/backend/core/protocols/__init__.py
@@ -4,25 +4,20 @@ Protocol handlers package.
 
 from .base import BaseProtocolHandler
 from .ssh import SSHHandler
-from .http import HTTPHandler
 from .ftp import FTPHandler
 from .telnet import TelnetHandler
 from .smtp import SMTPHandler
-from .https import HTTPSHandler
 
 PROTOCOL_MAP = {
     "ssh": SSHHandler,
-    "http": HTTPHandler,
     "ftp": FTPHandler,
     "telnet": TelnetHandler,
     "smtp": SMTPHandler,
-    "https": HTTPSHandler,
 }
 
 __all__ = [
     "BaseProtocolHandler",
     "SSHHandler",
-    "HTTPHandler",
     "FTPHandler",
     "TelnetHandler",
     "SMTPHandler",
